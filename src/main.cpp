aafe7e647686c1bf690fecdb5a011a8d436d73e2
Jose Antonio Martinez Vidaurre <jbmw16@gmail.com>
2025-08-21 21:22:19 -0400

    set default reading values for sensors

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,38 @@
 #include <Mouse.h>
 
 #define ENABLEPIN 2
+#define XPIN A0
+#define YPIN A1
+
+int centerX = 571;
+int centerY = 547;
+
+int scaleAxis(int reading) {
+
+
+
+  return 0;
+}
+
+
+void disableRoutine() {
+  int x = 0;
+  int y = 0;
+
+  Serial.begin(9600);
+  while (true) {
+  x = analogRead(XPIN);
+  y = analogRead(YPIN);
+
+  Serial.print(x); 
+  Serial.print("  ");
+  Serial.println(y);
+
+  delay(100);
+  }
+
+
+}
 
 void setup()
 {
@@ -11,33 +43,23 @@ void setup()
   pinMode(A1, INPUT);
 }
 
-void loop()
-{
+void loop() {
+  int x = 0;
+  int y = 0;
+
+
   if (!digitalRead(ENABLEPIN))
-    while (true) {
-      delay(1000);
-    }
+    disableRoutine();
 
 
+  Mouse.begin();
   while (true) {
-
-
+    x = scaleAxis(analogRead(XPIN));
+    y = scaleAxis(analogRead(YPIN));
 
 
   }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
+
